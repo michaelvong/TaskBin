98cedc3ca82904425d1ae0125c5ed6039be8d339
michaelvong <mvong006@ucr.edu>
2025-11-19 13:35:06 -0800

    fixed typo

--- CreateScripts/routes/update_task_status.py
+++ CreateScripts/routes/update_task_status.py
@@ -4,5 +4,5 @@ from TaskBin.CreateScripts.route_utils import RouteIntegration
 integration = RouteIntegration()
 integration.create_route(
     route_key="PATCH /boards/{board_id}/tasks/{task_id}",
-    lambda_name="TaskBin_ListUserTasks"
+    lambda_name="TaskBin_UpdateTaskStatus"
 )
