729931fe213a6199d2cde3925d126358fe5b6720
michaelvong <mvong006@ucr.edu>
2025-11-22 01:25:09 -0800

    fixed typo

--- CreateScripts/routes/list_board_members.py
+++ CreateScripts/routes/list_board_members.py
@@ -3,6 +3,6 @@ from TaskBin.CreateScripts.route_utils import RouteIntegration
 
 integration = RouteIntegration()
 integration.create_route(
-    route_key="GET /boards/{boardId}/members",
+    route_key="GET /boards/{board_id}/members",
     lambda_name="TaskBin_ListBoardMembers"
 )
